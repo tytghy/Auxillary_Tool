d21c11ed8e9ea8ebacdec2c56d85bf0da32ccf25
Louis <louisliu614@gmail.com>
2019-12-12 14:49:36 +0800

    Update im2tiff.m

--- im2tiff.m
+++ im2tiff.m
@@ -1,4 +1,7 @@
 function im2tiff(img,filename)
+%IM2TIFF Exports tif stack.
+%   IM2TIFF(img, filename) exports tif stack from a 3D image array. 
+%   The filename should be ended with ".tif".
 
 for i = 1:size(img,3)
     im = img(:,:,i);
